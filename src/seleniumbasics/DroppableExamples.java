4134799cf74702c5afce6f5de680ca5e33070923
musthaqahamed53 <musthaq53ahamed@gmail.com>
2022-06-22 12:42:57 +0530

    new

--- src/seleniumbasics/DroppableExamples.java
+++ src/seleniumbasics/DroppableExamples.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 public class DroppableExamples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.setProperty("webdriver.chrome.driver","D:\\CTS\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://leafground.com/pages/drop.html");
